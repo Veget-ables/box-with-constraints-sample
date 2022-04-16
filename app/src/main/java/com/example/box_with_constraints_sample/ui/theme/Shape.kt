5c4532654956f73608821ff8caafbe4afd6030ec
Veget-ables <makuhari109@gmail.com>
2022-04-16 14:10:51 +0900

    first commit

---
+++ app/src/main/java/com/example/box_with_constraints_sample/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.example.box_with_constraints_sample.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
